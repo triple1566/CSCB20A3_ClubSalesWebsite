65606782f7a464b7676b6d0db780d88b085da9d3
the creator <chenxi.yan@gmail.com>
2023-04-11 22:19:35 -0400

    checkout ...

--- RestaurantProject/static/js/orders/MyCart.js
+++ RestaurantProject/static/js/orders/MyCart.js
@@ -5,3 +5,9 @@ const cartItemsElement = document.getElementById('cart-items');
 const cartItemElement = document.createElement('li');
 cartItemElement.textContent = data;
 cartItemsElement.appendChild(cartItemElement);
+
+function checkoutAlert() {
+    alert("Checkout ... but not your normal checkout. Check out my github!");
+    window.location.href = "https://github.com/chrisyan04";
+  }
+  
